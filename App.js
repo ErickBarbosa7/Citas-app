3a0a4cfc9fbcacea48fa0de662d27888b81abdb8
Erick Barbosa <l.bace040211@itses.edu.mx>
2026-05-26 10:55:45 -0600

    clase 26 de mayo 2026

--- App.js
+++ App.js
@@ -1,20 +1,78 @@
+// 1. IMPORTANTE: Debes importar useState desde 'react'
+import { useState } from 'react'; 
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// 2. IMPORTANTE: Agregamos 'Modal' a las importaciones de react-native
+import { Pressable, StyleSheet, Text, SafeAreaView, Modal } from 'react-native';
 
 export default function App() {
+  const [modalVisible, setModalVisible] = useState(false);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={styles.titulo}>
+        Administrador de citas {" "}
+      </Text>
+
+      <Text style={styles.tituloBold}>
+        Veterinaria
+      </Text>
+      
+      {/* Conecté tu estado modalVisible al Modal para que funcione correctamente */}
+      <Modal visible={modalVisible} animationType="slide">
+        <SafeAreaView style={styles.container}>
+          <Text style={styles.titulo}>Este es un modal</Text>
+          <Pressable 
+            style={styles.btnNuevaCita} 
+            onPress={() => setModalVisible(false)}
+          >
+            <Text style={styles.btnTextoNuevaCita}> Cerrar modal</Text>
+          </Pressable>
+        </SafeAreaView>
+      </Modal>
+
+      <Pressable 
+        style={styles.btnNuevaCita} 
+        onPress={() => setModalVisible(true)}
+      >
+        <Text style={styles.btnTextoNuevaCita}>
+          Nueva cita
+        </Text>
+      </Pressable>
+      
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#F3F4F6',
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 40 // Añadido para dar un poco de margen superior si usas el SafeAreaView antiguo
   },
+  titulo: {
+    textAlign: 'center',
+    fontSize: 30,
+    color: '#374151',
+    fontWeight: '600'
+  },
+  tituloBold: {
+    fontWeight: '900',
+    color: '#6D28D9',
+    textAlign: 'center'
+  },
+  btnNuevaCita: {
+    backgroundColor: '#6D28D9',
+    padding: 15,
+    marginTop: 30,
+    marginHorizontal: 20,
+    borderRadius: 10
+  },
+  btnTextoNuevaCita: {
+    textAlign: 'center',
+    color: '#FFF',
+    fontSize: 18,
+    fontWeight: '900',
+    textTransform: 'uppercase'
+  }
 });
