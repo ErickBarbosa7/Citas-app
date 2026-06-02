ca33d92fc2897dff69a2ce27bc1c2c30b8ef945e
Erick Barbosa <l.bace040211@itses.edu.mx>
2026-06-01 20:56:27 -0600

    clase 1 Junio 2026

--- App.js
+++ App.js
@@ -1,62 +1,42 @@
 import { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import {Pressable, StyleSheet, Text, SafeAreaView, Modal, View, TextInput} from 'react-native';
+import { Pressable, StyleSheet, Text, SafeAreaView } from 'react-native';
+import Formulario from './components/Formulario';
 
 export default function App() {
   const [modalVisible, setModalVisible] = useState(false);
-  const [nombrePaciente, setNombrePaciente] = useState('');
+  
+  const [paciente, setPaciente] = useState('');
+  const [pacientes, setPacientes] = useState([]);
+
+  const cerrarModal = () => {
+      setModalVisible(false);
+  }
 
   return (
     <SafeAreaView style={styles.container}>
       <StatusBar style="auto" />
-
+      
       <Text style={styles.titulo}>
         Administrador de citas
       </Text>
-
+      
       <Text style={styles.tituloBold}>
         Veterinaria
       </Text>
 
-      <Modal
-        visible={modalVisible}
-        animationType="slide"
-      >
-        <View style={styles.formulario}>
-          <View style={styles.campo}>
-            <Text style={styles.label}>
-              Nombre Paciente
-            </Text>
-
-            <TextInput
-              style={styles.input}
-              placeholder="Nombre paciente"
-              placeholderTextColor="#666"
-              value={nombrePaciente}
-              onChangeText={setNombrePaciente}
-              maxLength={20}
-            />
-
-            <Pressable
-              style={styles.btnCerrar}
-              onPress={() => setModalVisible(false)}
-            >
-              <Text style={styles.btnTextoCerrar}>
-                Cerrar modal
-              </Text>
-            </Pressable>
-          </View>
-        </View>
-      </Modal>
-
       <Pressable
         style={styles.btnNuevaCita}
-        onPress={() => setModalVisible(true)}
-      >
+        onPress={() => setModalVisible(true)}>
+
         <Text style={styles.btnTextoNuevaCita}>
           Nueva cita
         </Text>
       </Pressable>
+
+      <Formulario 
+        modalVisible={modalVisible} cerrarModal={cerrarModal} pacientes={pacientes} setPacientes={setPacientes}
+      />
     </SafeAreaView>
   );
 }
@@ -67,14 +47,12 @@ const styles = StyleSheet.create({
     backgroundColor: '#F3F4F6',
     padding: 20
   },
-
   titulo: {
     textAlign: 'center',
     fontSize: 28,
     fontWeight: '600',
     marginTop: 40
   },
-
   tituloBold: {
     textAlign: 'center',
     fontSize: 32,
@@ -82,56 +60,15 @@ const styles = StyleSheet.create({
     color: '#6D28D9',
     marginBottom: 40
   },
-
-  formulario: {
-    flex: 1,
-    backgroundColor: '#6D28D9',
-    padding: 20
-  },
-
-  campo: {
-    marginTop: 30
-  },
-
-  label: {
-    color: '#FFF',
-    fontSize: 18,
-    fontWeight: '700',
-    marginBottom: 10
-  },
-
-  input: {
-    backgroundColor: '#FFF',
-    borderRadius: 10,
-    padding: 15,
-    fontSize: 16
-  },
-
   btnNuevaCita: {
     backgroundColor: '#6D28D9',
     padding: 15,
     borderRadius: 10
   },
-
   btnTextoNuevaCita: {
     color: '#FFF',
     textAlign: 'center',
     fontSize: 18,
     fontWeight: '700'
-  },
-
-  btnCerrar: {
-    backgroundColor: '#F59E0B',
-    padding: 15,
-    marginTop: 30,
-    borderRadius: 10
-  },
-
-  btnTextoCerrar: {
-    textAlign: 'center',
-    color: '#FFF',
-    fontSize: 18,
-    fontWeight: '900',
-    textTransform: 'uppercase'
   }
 });
--- components/Formulario.js
+++ components/Formulario.js
@@ -1,24 +1,90 @@
 import { View, Text, Modal, SafeAreaView, ScrollView, Pressable, TextInput, StyleSheet } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 
-const Formulario = () => {
-    const [id, setId] = useState('')
-    const [paciente, setPaciente] = useState('')
-    const [email, setEmail] = useState('')
+const Formulario = ({ modalVisible, cerrarModal, pacientes, setPacientes }) => {
+    const [paciente, setPaciente] = useState('');
+    const [email, setEmail] = useState('');
+    const [telefono, setTelefono] = useState('');
+    const [fecha, setFecha] = useState('');
+    const [sintomas, setSintomas] = useState('');
+
+    const handleCita = () => {
+        if([paciente, email, telefono, fecha, sintomas].includes('')) {
+            console.log('Hay campos vacios');
+            return;
+        }
+
+        const nuevoPaciente = {
+            id: Date.now().toString(),
+            paciente,
+            email,
+            telefono,
+            fecha,
+            sintomas
+        };
+
+        setPacientes([...pacientes, nuevoPaciente]);
+        setPaciente('');
+        setEmail('');
+        setTelefono('');
+        setFecha('');
+        setSintomas('');
+
+        // Cerrar el modal
+        cerrarModal();
+    };
+
     return (
-        <Modal>
-            <SafeAreaView>
+        <Modal 
+            animationType="slide" 
+            visible={modalVisible}
+        >
+            <SafeAreaView style={styles.formulario}>
                 <ScrollView>
-                    <Text> Nueva Cita</Text>
-                    <Pressable>
-                        <Text> X Cancelar</Text>
+                    <Text style={styles.titulo}>Nueva <Text style={styles.tituloBold}>Cita</Text></Text>
+                    
+                    <Pressable 
+                        style={styles.btnCancelar}
+                        onPress={cerrarModal} 
+                    >
+                        <Text style={styles.btnCancelarTexto}>X Cancelar</Text>
                     </Pressable>
-                    <View>
-                        <Text>Nombre del Paciente</Text>
-                        <TextInput/>
+                    
+                    <View style={styles.campo}>
+                        <Text style={styles.label}>Nombre del Paciente</Text>
+                        <TextInput  style={styles.input} placeholder="Nombre Paciente" placeholderTextColor="#666" value={paciente} onChangeText={setPaciente}
+                        />
                     </View>
-                    <Pressable>
-                        <Text> Guardar</Text>
+
+                    <View style={styles.campo}>
+                        <Text style={styles.label}>Email del Paciente</Text>
+                        <TextInput  style={styles.input} placeholder="Email" placeholderTextColor="#666" keyboardType="email-address" value={email} onChangeText={setEmail}
+                        />
+                    </View>
+
+                    <View style={styles.campo}>
+                        <Text style={styles.label}>Teléfono</Text>
+                        <TextInput style={styles.input} placeholder="Teléfono" placeholderTextColor="#666" keyboardType="phone-pad" maxLength={10} value={telefono} onChangeText={setTelefono}/>
+                    </View>
+
+                    <View style={styles.campo}>
+                        <Text style={styles.label}>Fecha</Text>
+                        <TextInput style={styles.input} placeholder="DD/MM/AAAA" placeholderTextColor="#666" value={fecha} onChangeText={setFecha}
+                        />
+                    </View>
+
+                    <View style={styles.campo}>
+                        <Text style={styles.label}>Síntomas</Text>
+                        <TextInput 
+                            style={[styles.input, { height: 100, textAlignVertical: 'top' }]} placeholder="Describe los síntomas" placeholderTextColor="#666" value={sintomas} onChangeText={setSintomas} multiline={true} numberOfLines={4}
+                        />
+                    </View>
+                    
+                    <Pressable 
+                        style={styles.btnNuevaCita}
+                        onPress={handleCita}
+                    >
+                        <Text style={styles.btnNuevaCitaTexto}>Guardar</Text>
                     </Pressable>
                 </ScrollView>
             </SafeAreaView>
@@ -26,56 +92,66 @@ const Formulario = () => {
     );
 }
 
-const style = StyleSheet.create({
-    campo:{
-      marginTop:10,
-      marginHorizontal:30
-  },
-  titulo: {
-    textAlign: 'center',
-    fontSize: 30,
-    color: '#374151',
-    fontWeight: '600'
-  },
-  tituloBold: {
-    fontWeight: '900',
-    color: '#6D28D9',
-    textAlign: 'center'
-  },
-  btnNuevaCita: {
-    backgroundColor: '#6D28D9',
-    padding: 15,
-    marginTop: 30,
-    marginHorizontal: 20,
-    borderRadius: 10
-  },
-  btnTextoNuevaCita: {
-    textAlign: 'center',
-    color: '#FFF',
-    fontSize: 18,
-    fontWeight: '900',
-    textTransform: 'uppercase'
-  },
-  contenidoModal: {
-    flex: 1,
-    backgroundColor: '#FFF',
-  },
-  label: {
-    color: '#374151', 
-    marginBottom: 10, 
-    marginTop: 40,
-    fontSize: 20,
-    fontWeight: '600',
-    marginHorizontal: 20,
-  },
-  input: {
-    backgroundColor: '#F3F4F6',
-    padding: 15,
-    borderRadius: 10,
-    marginHorizontal: 20,
-    marginTop: 10,
-    fontSize: 16
-  },
-})
+const styles = StyleSheet.create({
+titulo: {
+fontSize: 30,
+fontWeight: "600",
+textAlign: "center",
+marginTop: 30,
+color: "#FFF",
+},
+tituloBold: {
+fontWeight: "900",
+},
 
-export default Formulario
+campo: {
+marginTop: 10,
+marginHorizontal: 30,
+},
+label: {
+color: "#FFF",
+marginBottom: 10,
+marginTop: 15,
+fontSize: 20,
+fontWeight: "600",
+},
+input: {
+backgroundColor: "#FFF",
+padding: 15,
+borderRadius: 10,
+},
+formulario: {
+backgroundColor: "#6D28D9",
+flex: 1,
+},
+btnCancelar: {
+marginVertical: 30,
+backgroundColor: "#5827A4",
+marginHorizontal: 30,
+padding: 15,
+borderRadius: 10,
+},
+btnCancelarTexto: {
+color: "#FFF",
+textAlign: "center",
+fontWeight: "900",
+fontSize: 16,
+textTransform: "uppercase",
+},
+btnNuevaCita: {
+marginVertical: 50,
+backgroundColor: "#F59E0B",
+paddingVertical: 15,
+marginHorizontal: 30,
+borderRadius: 10,
+},
+btnNuevaCitaTexto: {
+color: "#5827A4",
+textAlign: "center",
+fontWeight: "900",
+fontSize: 16,
+textTransform: "uppercase",
+},
+});
+
+export default Formulario;
